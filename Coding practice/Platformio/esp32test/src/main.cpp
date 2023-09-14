687ee2ecf02cdad2475877bc3ac76fcad4ee934c
kelvinjdel <kelvinjdel@gmail.com>
2023-09-14 13:09:57 -0400

    update

--- Coding practice/Platformio/esp32test/src/main.cpp
+++ Coding practice/Platformio/esp32test/src/main.cpp
@@ -1,15 +1,55 @@
-
 #include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
- #define A 2
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define NUMFLAKES     10 // Number of snowflakes in the animation example
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+static const unsigned char PROGMEM logo_bmp[] =
+{ 0b00000000, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000011, 0b11100000,
+  0b11110011, 0b11100000,
+  0b11111110, 0b11111000,
+  0b01111110, 0b11111111,
+  0b00110011, 0b10011111,
+  0b00011111, 0b11111100,
+  0b00001101, 0b01110000,
+  0b00011011, 0b10100000,
+  0b00111111, 0b11100000,
+  0b00111111, 0b11110000,
+  0b01111100, 0b11110000,
+  0b01110000, 0b01110000,
+  0b00000000, 0b00110000 };
+
+ #define A 5
  #define B 4
  #define sw 15
 
 unsigned long _lastIncReadTime= micros();
 unsigned long _lastDecReadTime= micros();
 int _pauseLength =25000;
-volatile int counter =0;
+volatile int counter =9;
+const String KeySignatureSharp[12] = {"E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#"};
+const String KeySignatureFlat[12] = {"E", "F", "Gb", "G", "Ab", "A", "Bb", "B", "C", "Db", "D", "Eb"};
 
+void testtext();
 void read_encoder();
 
 void setup() { 
@@ -20,6 +60,26 @@ void setup() {
    Serial.begin (9600);
    // Reads the initial state of the A
    Serial.println("initiating");
+   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+   display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  // Draw a single pixel in white
+  display.drawPixel(10, 10, SSD1306_WHITE);
+
+  // Show the display buffer on the screen. You MUST call display() after
+  // drawing commands to make them visible on screen!
+  display.display();
+  delay(2000);
+
+
+  testtext();    
 } 
 
 void loop() { 
@@ -28,8 +88,23 @@ void loop() {
   {
     Serial.println(counter);
     lastCounter= counter;
+    testtext();
   }
 }
+
+void testtext(void) {
+  display.clearDisplay();
+
+  display.setTextSize(2); // Draw 2X-scale text
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(10, 0);
+  String keysignature= KeySignatureFlat[counter];
+  display.println((keysignature));
+  display.display();      // Show initial text
+  delay(100);
+
+}
+
 
 void read_encoder() //IRS, change in A or B causes this interrupt
 {
@@ -65,4 +140,10 @@ void read_encoder() //IRS, change in A or B causes this interrupt
     counter=counter + changevalue;
     encval=0;
   } 
-}
+  if (counter >=12)
+  {counter=12;
+  }
+  else if (counter<=0)
+  {counter=0;
+  }
+  }
